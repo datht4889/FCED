88fb54f210548df89bb7e55817e073d7b193ba4b
datht4889 <hoangdat18112003@gmail.com>
2025-03-13 09:06:30 +0700

    Add ASAM optimizer implementation and update training script to use ASAM

--- FCED/sam.py
+++ FCED/sam.py
@@ -1,4 +1,7 @@
 import torch
+from collections import defaultdict
+import torch.nn.functional as F
+import random
 
 
 class SAM(torch.optim.Optimizer):
@@ -249,3 +252,275 @@ class TRAM(torch.optim.Optimizer):
     def load_state_dict(self, state_dict):
         super().load_state_dict(state_dict)
         self.base_optimizer.param_groups = self.param_groups 
+
+class ASAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.5, eta=0.01, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+
+        defaults = dict(rho=rho, eta=eta, **kwargs)
+        super(ASAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+        self.defaults.update(self.base_optimizer.defaults)
+        self.state = defaultdict(dict)
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        wgrads = []
+        for group in self.param_groups:
+            for n, p in group["params"]:
+                if p.grad is None:
+                    continue
+                t_w = self.state[p].get("eps")
+                if t_w is None:
+                    t_w = torch.clone(p).detach()
+                    self.state[p]["eps"] = t_w
+                if 'weight' in n:
+                    t_w[...] = p[...]
+                    t_w.abs_().add_(self.defaults["eta"])
+                    p.grad.mul_(t_w)
+                wgrads.append(torch.norm(p.grad, p=2))
+        wgrad_norm = torch.norm(torch.stack(wgrads), p=2) + 1.e-16
+        
+        for group in self.param_groups:
+            for n, p in group["params"]:
+                if p.grad is None:
+                    continue
+                t_w = self.state[p].get("eps")
+                if 'weight' in n:
+                    p.grad.mul_(t_w)
+                eps = t_w
+                eps[...] = p.grad[...]
+                eps.mul_(group["rho"] / wgrad_norm)
+                p.add_(eps)
+        
+        if zero_grad:
+            self.zero_grad()
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for n, p in group["params"]:
+                if p.grad is None:
+                    continue
+                p.sub_(self.state[p]["eps"])
+        self.base_optimizer.step()
+        if zero_grad:
+            self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        assert closure is not None, "ASAM requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def load_state_dict(self, state_dict):
+        super().load_state_dict(state_dict)
+        self.base_optimizer.param_groups = self.param_groups
+
+
+def centralized_gradient(x,use_gc=True,gc_conv_only=False):
+    if use_gc:
+      if gc_conv_only:
+        if len(list(x.size()))>3:
+            x.add_(-x.mean(dim = tuple(range(1,len(list(x.size())))), keepdim = True))
+      else:
+        if len(list(x.size()))>1:
+            x.add_(-x.mean(dim = tuple(range(1,len(list(x.size())))), keepdim = True))
+    return x
+
+class GCSAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, adaptive=False, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+
+        defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
+        super(GCSAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None: continue
+                #GC operation
+                p.grad =centralized_gradient(p.grad ,use_gc=True,gc_conv_only=False)
+                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+                self.state[p]["e_w"] = e_w
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+                #p.grad =centralized_gradient(p.grad ,use_gc=True,gc_conv_only=False)
+                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
+
+class ESAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05,beta=1.0,gamma=1.0,adaptive=False,**kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+        self.beta = beta
+        self.gamma = gamma
+
+        defaults = dict(rho=rho,adaptive=adaptive, **kwargs)
+        super(ESAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer
+        self.param_groups = self.base_optimizer.param_groups
+        for group in self.param_groups:
+            group["rho"] = rho
+            group["adaptive"] = adaptive
+        self.paras = None
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        #first order sum 
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-7) / self.beta
+            for p in group["params"]:
+                p.requires_grad = True 
+                if p.grad is None: continue
+                #original sam 
+                # e_w = p.grad * scale.to(p)
+                #asam 
+                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+                p.add_(e_w * 1)  # climb to the local maximum "w + e(w)"
+                self.state[p]["e_w"] = e_w
+
+
+
+        if zero_grad: self.zero_grad()
+
+    '''
+    @torch.no_grad()
+    def first_half(self, zero_grad=False):
+        #first order sum 
+        for group in self.param_groups:
+            for p in group["params"]:
+                if self.state[p]:
+                    p.add_(self.state[p]["e_w"]*0.90)  # climb to the local maximum "w + e(w)"
+    '''
+
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None or not self.state[p]: continue
+                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+                self.state[p]["e_w"] = 0
+
+                if random.random() > self.beta:
+                    p.requires_grad = False
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad: self.zero_grad()
+
+    def step(self):
+        inputs,targets,loss_fct,model,defined_backward = self.paras
+        assert defined_backward is not None, "Sharpness Aware Minimization requires defined_backward, but it was not provided"
+
+        model.require_backward_grad_sync = False
+        model.require_forward_param_sync = True
+
+
+        logits = model(inputs)
+        loss = loss_fct(logits,targets)
+
+        l_before = loss.clone().detach()
+        predictions = logits
+        return_loss = loss.clone().detach()
+        loss = loss.mean()
+        defined_backward(loss)
+
+        #first step to w + e(w)
+        self.first_step(True)
+
+
+        with torch.no_grad():
+            l_after = loss_fct(model(inputs),targets)
+            instance_sharpness = l_after-l_before
+
+            #codes for sorting 
+            prob = self.gamma
+            if prob >=0.99:
+                indices = range(len(targets))
+            else:
+                position = int(len(targets) * prob)
+                cutoff,_ = torch.topk(instance_sharpness,position)
+                cutoff = cutoff[-1]
+
+                # cutoff = 0
+                #select top k% 
+
+                indices = [instance_sharpness > cutoff] 
+
+
+        # second forward-backward step
+        # self.first_half()
+
+        model.require_backward_grad_sync = True
+        model.require_forward_param_sync = False
+
+
+
+        loss = loss_fct(model(inputs[indices]), targets[indices])
+        loss = loss.mean()
+        defined_backward(loss)
+        self.second_step(True)
+
+        self.returnthings = (predictions,return_loss)
+ 
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        #original sam 
+                        # p.grad.norm(p=2).to(shared_device)
+                        #asam 
+                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
--- FCED/train.py
+++ FCED/train.py
@@ -70,9 +70,8 @@ def train(local_rank, args):
     model.to(device)
     optimizer = AdamW(model.parameters(), lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999)) #TODO: Hyper parameters
     if args.sam:
-            # base_optimizer = AdamW
-            # optimizer = FriendlySAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, adaptive=True, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
-            optimizer = FriendlySAM(params=model.parameters(), base_optimizer=SGD, rho=args.rho, adaptive=True, lr=args.lr, weight_decay=args.decay, momentum=0.9, nesterov=False)
+            base_optimizer = AdamW
+            optimizer = ASAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, adaptive=True, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
     # if args.amp:
         # model, optimizer = amp.initialize(model, optimizer, opt_level="O1") 
     if args.parallel == 'DDP':
