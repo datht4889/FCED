28280172dc5e6bdfc936ab95df3dd2dcaa239683
datht4889 <hoangdat18112003@gmail.com>
2025-03-17 08:48:36 +0700

    Update train.py

--- FCED/train.py
+++ FCED/train.py
@@ -596,15 +596,15 @@ def train(local_rank, args):
                             # loss = loss * (1 - w) + (loss_fd + loss_pd) * w
                             # loss.backward()
                             loss_list = [loss, loss_fd, loss_pd]
-                            alpha_w = 1 
-                            loss_list = [alpha_w * loss_list[0] + sum(loss_list), alpha_w * loss_list[1] + sum(loss_list), alpha_w * loss_list[2] + sum(loss_list)]
+                            # alpha_w = 1 
+                            # loss_list = [alpha_w * loss_list[0] + sum(loss_list), alpha_w * loss_list[1] + sum(loss_list), alpha_w * loss_list[2] + sum(loss_list)]
 
                         else:
                             # loss = loss + args.alpha * loss_fd + args.beta * loss_pd
                             # loss_list = [loss, args.alpha * loss_fd, args.beta * loss_pd]
                             loss_list = [loss, loss_fd, loss_pd]
-                            alpha_w = 1
-                            loss_list = [alpha_w * loss_list[0] + sum(loss_list), alpha_w * loss_list[1] + sum(loss_list), alpha_w * loss_list[2] + sum(loss_list)]
+                            # alpha_w = 1
+                            # loss_list = [alpha_w * loss_list[0] + sum(loss_list), alpha_w * loss_list[1] + sum(loss_list), alpha_w * loss_list[2] + sum(loss_list)]
 
                         if args.mul_task_type == 'PCGrad':
                             mul_loss = PCGrad(
