ef5c422fc13ff5c0044d8141629ae125e86ad47b
datht4889 <hoangdat18112003@gmail.com>
2025-03-17 08:50:39 +0700

    Update train.py

--- FCED/train.py
+++ FCED/train.py
@@ -73,7 +73,8 @@ def train(local_rank, args):
             base_optimizer = AdamW
             # optimizer = SAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, adaptive=True, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
             # optimizer = ASAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
-            optimizer = ESAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, beta=1.0,gamma=1.0, adaptive=True, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
+            # optimizer = ESAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, beta=1.0,gamma=1.0, adaptive=True, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
+            optimizer = GCSAM(params=model.parameters(), base_optimizer=base_optimizer, rho=args.rho, adaptive=True, lr=args.lr, weight_decay=args.decay, eps=args.adamw_eps, betas=(0.9, 0.999))
     # if args.amp:
         # model, optimizer = amp.initialize(model, optimizer, opt_level="O1") 
     if args.parallel == 'DDP':
