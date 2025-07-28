e7e784a5eb2720178d1b3b06431f5bb9f1c0959b
datht4889 <hoangdat18112003@gmail.com>
2025-07-28 22:03:13 +0700

    SAM 0.01 decay

--- FCRE/CPL-mmi/train.py
+++ FCRE/CPL-mmi/train.py
@@ -619,6 +619,10 @@ if __name__ == '__main__':
     print('his_acc mean: ', np.around(ave, 4))
     logger.info('----------END')
     logger.info(f'his_acc mean: {np.around(ave, 4)*100}')
+    logger.info(f'SAM: {config.SAM}')
+    logger.info(f'SAM_type: {config.SAM_type}')
+    logger.info(f'SAM Optimizer: {config.sam_optimizer}')
+    logger.info(f'decay: {config.decay}')
 
 
 
