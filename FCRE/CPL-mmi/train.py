29eb8ad6f0bbc9e1ec40f05b0a7cd6f4196dc8e4
datht4889 <hoangdat18112003@gmail.com>
2025-07-30 13:49:21 +0700

    CPLMMI base_optim = Adam

--- FCRE/CPL-mmi/train.py
+++ FCRE/CPL-mmi/train.py
@@ -223,11 +223,11 @@ class Manager(object):
         print('')           
     def train_model_mixup(self, encoder, training_data):
         data_loader = get_data_loader_BERT(self.config, training_data, shuffle=True)
-        optimizer = optim.AdamW(params=encoder.parameters(), lr=self.config.lr)
+        optimizer = optim.Adam(params=encoder.parameters(), lr=self.config.lr)
         if self.config.SAM:
             # base_optimizer = optim.Adam
             # optimizer = SAM(params=encoder.parameters(), base_optimizer=base_optimizer, rho=self.config.rho, adaptive=True, lr=self.config.lr)
-            base_optimizer = optim.AdamW
+            base_optimizer = optim.Adam
             if self.config.sam_optimizer=='SAM':
                 optimizer = SAM(params=encoder.parameters(), base_optimizer=base_optimizer, rho=self.config.rho, adaptive=True, lr=self.config.lr, weight_decay=self.config.decay, betas=(0.9, 0.999))
             elif self.config.sam_optimizer=='ASAM':
