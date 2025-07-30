407000c00c43aacda26330850f5a64b250a9f78b
datht4889 <hoangdat18112003@gmail.com>
2025-07-30 15:49:15 +0700

    add log

--- FCRE/CPL-mmi/train.py
+++ FCRE/CPL-mmi/train.py
@@ -514,7 +514,7 @@ if __name__ == '__main__':
     parser.add_argument("--epoch_mem", default=5, type=int)
     parser.add_argument("--mixup_loss_1", default=0.25, type=float)
     parser.add_argument("--mixup_loss_2", default=0.25, type=float)
-    parser.add_argument("--SAM", action = 'store_true')
+    parser.add_argument("--SAM", action = 'store_true', default=False)
     parser.add_argument("--sam_optimizer", default="SAM", type=str)
     parser.add_argument("--rho", default=0.05, type=float)
     parser.add_argument("--decay", default=0, type=float)
