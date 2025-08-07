df0976ba98a69c046c06af4d73480eaae109f0ff
datht4889 <hoangdat18112003@gmail.com>
2025-08-07 08:38:43 +0700

    bert sampler

--- FCRE/CPL/train.py
+++ FCRE/CPL/train.py
@@ -15,7 +15,7 @@ import os
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 
 from config import Config
-from sampler_bert_llm import data_sampler_CFRL
+from sampler import data_sampler_CFRL
 from data_loader import get_data_loader_BERTLLM, get_data_loader_BERT
 from utils_llm import Moment_LLM, gen_data
 from utils import Moment, gen_data
