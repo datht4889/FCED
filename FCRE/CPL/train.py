fdb00f0f7e1e10826029c3b5ff98fe6b8f4d4e03
datht4889 <hoangdat18112003@gmail.com>
2025-08-08 10:52:40 +0700

    set seed in train()

--- FCRE/CPL/train.py
+++ FCRE/CPL/train.py
@@ -382,6 +382,7 @@ class Manager(object):
 
 
     def train(self):
+        enable_full_determinism(config.seed)
         # sampler 
         sampler = data_sampler_CFRL(config=self.config, seed=self.config.seed)
         print('prepared data!')
@@ -620,6 +621,7 @@ if __name__ == '__main__':
     acc_list = []
     for i in range(config.total_round):
         config.seed = base_seed + i * 100
+        enable_full_determinism(config.seed)
         print('--------Round ', i)
         print('seed: ', config.seed)
         logger.info(f"--------Round {i}")
