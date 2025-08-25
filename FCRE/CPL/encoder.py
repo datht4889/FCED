34ff55eb7c7e94becb09ab4a73bc6c0080e1b445
datht4889 <hoangdat18112003@gmail.com>
2025-08-25 09:19:34 +0700

    average attention

--- FCRE/CPL/encoder.py
+++ FCRE/CPL/encoder.py
@@ -156,7 +156,8 @@ class EncodingModel(nn.Module):
 
         if is_distill:
             # attentions_layer: (B, H, S, S)
-            attentions_layer = attention_scores[-1]  # choose layer (e.g., last layer)
+            # attentions_layer = attention_scores[-1]  # choose layer (e.g., last layer)
+            attentions_layer = torch.stack(attention_scores, dim=0) # averaged-layer attentions
 
             # Column-wise sum over queries (dim=2). Result: (B, H, S)
             col_sum_per_head = attentions_layer.sum(dim=2)
