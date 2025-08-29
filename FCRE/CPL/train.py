b318ea497d2ffa6faf764eb94bed8e3469d049bd
datht4889 <hoangdat18112003@gmail.com>
2025-08-29 09:07:31 +0700

    fix

--- FCRE/CPL/train.py
+++ FCRE/CPL/train.py
@@ -185,6 +185,7 @@ class Manager(object):
                         optimizer.first_step(zero_grad=True, rho=distillation_rho)
                     else:
                         optimizer.first_step(zero_grad=True, rho=self.config.rho)
+                        
                     if self.config.use_llm:
                         hidden = encoder(instance['input'])
                     else:
