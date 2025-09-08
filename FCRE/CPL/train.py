fe2e0beef39e8e88c27081e18ad0b58899352bf4
datht4889 <hoangdat18112003@gmail.com>
2025-09-08 23:28:43 +0700

    add json few and utils

--- FCRE/CPL/train.py
+++ FCRE/CPL/train.py
@@ -374,7 +374,7 @@ class Manager(object):
                 for rel in current_relations:
                     for sample in memory_samples[rel]:
                         sample_text = self._get_sample_text(self.config.training_data, sample['index'])
-                        gen_samples = gen_data(self.config.task_name, self.r2desc, self.rel2id, sample_text, self.config.num_gen, self.config.gpt_temp, self.config.key)
+                        gen_samples = gen_data(self.config.task_name, self.r2desc, self.rel2id, sample_text, self.config.num_gen, self.config.gpt_temp, self.config.current_round)
                         gen_text += gen_samples
                 for sample in gen_text:
                     data_generation.append(sampler.tokenize(sample))
@@ -543,6 +543,7 @@ if __name__ == '__main__':
     acc_list = []
     for i in range(config.total_round):
         config.seed = base_seed + i * 100
+        config.current_round = i
         print('--------Round ', i)
         print('seed: ', config.seed)
         logger.info(f"--------Round {i}")
