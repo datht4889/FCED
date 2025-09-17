4578f2cdba20729641b32758300de0b39e2919e0
datht4889 <hoangdat18112003@gmail.com>
2025-09-17 17:41:46 +0700

    update

--- FCRE/CPL/encoder_llm.py
+++ FCRE/CPL/encoder_llm.py
@@ -491,6 +491,8 @@ class EncodingModel_LLM2vec(nn.Module):
             attention_mask=input_data['attention_mask'],
             output_attentions=True if is_distill else None
         )
+
+        breakpoint()
         
         # Get last hidden states
         last_hidden_state = outputs.last_hidden_state
